641ed783eb993cf2f871aae417115fe14de1eb0d
jaso69 <joseonieva@gmail.com>
2024-03-11 19:41:23 +0100

    op

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Options, Post } from '@nestjs/common';
 import { GptService } from './gpt.service';
 import { OrthographyDto } from './dtos';
-
 @Controller('gpt')
 export class GptController {
   constructor(private readonly gptService: GptService) {}
   @Post('check')
   check(@Body() orthographyDto: OrthographyDto) {
+    return this.gptService.orthographyCheck(orthographyDto);
+  }
+
+  @Options('check')
+  ok(@Body() orthographyDto: OrthographyDto) {
     return this.gptService.orthographyCheck(orthographyDto);
   }
 }
